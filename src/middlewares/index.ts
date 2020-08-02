2f33aeaa9d7b19d9a9ff6aa6301cd20175c623ff
Rahul <rahul.singh@smartsensesolutions.com>
2020-08-02 15:30:56 +0530

    CORS enabled

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,8 +1,10 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import { Application } from 'express';
+import cors from 'cors';
 
 export default (app: Application) => {
+  app.use(cors());
   app.use(compression());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
